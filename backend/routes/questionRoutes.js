91aafd757711accf5b1158cedbaffc8dab5662b7
Parviz Namazli <55592138+Perviz021@users.noreply.github.com>
2025-03-25 17:12:38 +0400

    Format questions for HTML by replacing newline characters with <br> tags in the response

--- backend/routes/questionRoutes.js
+++ backend/routes/questionRoutes.js
@@ -11,7 +11,13 @@ router.get("/questions/:subjectCode", authenticate, async (req, res) => {
       "SELECT * FROM questions WHERE `fənnin_kodu` = ?",
       [subjectCode]
     );
-    res.json(questions);
+    // res.json(questions);
+    res.json(
+      questions.map((q) => ({
+        ...q,
+        question: q.question.replace(/\n/g, "<br>"), // Replace \n with <br> for HTML
+      }))
+    );
   } catch (error) {
     console.error("Error fetching questions:", error);
     res.status(500).json({ error: "Internal Server Error" });
