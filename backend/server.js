b2e31692b9c7c4df18b58f419bf117844149a60c
Parviz Namazli <55592138+Perviz021@users.noreply.github.com>
2025-04-09 23:22:45 +0400

    Add review routes to the server for handling review-related API requests

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import questionRoutes from "./routes/questionRoutes.js";
 import examRoutes from "./routes/examRoutes.js";
 import resultRoutes from "./routes/resultRoutes.js";
 import completedExamsRoutes from "./routes/completedExamsRoutes.js";
+import reviewRoutes from "./routes/reviewRoutes.js";
 
 const app = express();
 app.use(cors());
@@ -17,6 +18,7 @@ app.use("/api", questionRoutes);
 app.use("/api", examRoutes);
 app.use("/api", resultRoutes);
 app.use("/api", completedExamsRoutes);
+app.use("/api", reviewRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
