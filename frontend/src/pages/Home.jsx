7a6862a61c667c0168536ae899ed1fe255cccc1d
Parviz Namazli <perviznamazli021@gmail.com>
2025-04-17 17:50:16 +0400

    Refactor Home component to include fullname in greeting and format date display for exam subjects

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,15 +1,19 @@
 import { Link, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
+import { formatDate } from "../utils/dateFormatter";
 
 const API_BASE = "http://localhost:5000/api";
 
 const Home = () => {
   const [subjects, setSubjects] = useState([]);
   const [completedExams, setCompletedExams] = useState(new Set()); // Store completed exams
+  const [fullname, setFullname] = useState();
   const navigate = useNavigate();
 
   useEffect(() => {
     const token = localStorage.getItem("token");
+    setFullname(localStorage.getItem("fullname"));
+
     if (!token) {
       navigate("/login");
       return;
@@ -42,12 +46,20 @@ const Home = () => {
     window.location.reload();
   };
 
+  const formattedString = (str) => {
+    if (!str) return "";
+    return str.slice(0, str.lastIndexOf(" ") + 1).trim();
+  };
+
   return (
     <div className="container mx-auto p-4">
       <div className="flex justify-between items-center mb-4">
-        <h2 className="text-2xl font-bold montserrat montserrat-700">
+        <div className="mb-2 text-xl font-semibold montserrat montserrat-700">
+          Xoş gəlmisiz, {formattedString(fullname)}!
+        </div>
+        {/* <h2 className="text-2xl font-bold montserrat montserrat-700">
           Bütün fənlər
-        </h2>
+        </h2> */}
         <button
           onClick={handleLogout}
           className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 cursor-pointer inter inter-500"
@@ -67,16 +79,19 @@ const Home = () => {
               ) : (
                 <Link
                   to={`/exam/${subject.id}`}
-                  className="p-4 bg-secondary text-white rounded-lg text-center hover:bg-main transition duration-300 ease-in-out flex items-center justify-center inter inter-600"
+                  className="p-4 bg-secondary text-white rounded-lg text-center hover:bg-main transition duration-300 ease-in-out flex items-center justify-center flex-col inter inter-600"
                 >
-                  {subject.name}
+                  <span>{subject.name}</span>
+                  <span>İmtahan vaxtı: {formatDate(1736467200000)}</span>
                 </Link>
               )}
             </div>
           ))}
         </div>
       ) : (
-        <p className="text-gray-600 text-center">No subjects available.</p>
+        <p className="text-gray-600 text-center">
+          Tələbənin fənləri mövcud deyil.
+        </p>
       )}
     </div>
   );
