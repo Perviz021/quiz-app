8f53daf4b8ca0baf4980bc205eae1571e5ac05f8
Parviz Namazli <perviznamazli021@gmail.com>
2025-04-23 11:37:41 +0400

    Update API_BASE default URL to use localhost

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,4 +1,3 @@
-const API_BASE =
-  import.meta.env.VITE_API_BASE || "http://192.168.11.78:5000/api";
+const API_BASE = import.meta.env.VITE_API_BASE || "http://localhost:5000/api";
 
 export default API_BASE;
