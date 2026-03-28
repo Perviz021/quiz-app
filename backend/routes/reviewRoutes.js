1af0396aa8b036b091f8308eb6aae8a25c2309f8
Parviz Namazli <55592138+Perviz021@users.noreply.github.com>
2026-03-29 00:50:38 +0400

    Enhance reviewRoutes to include additional question and option images in the review query response

--- backend/routes/reviewRoutes.js
+++ backend/routes/reviewRoutes.js
@@ -14,18 +14,24 @@ router.get("/review/:subjectCode", authenticate, async (req, res) => {
       SELECT 
         q.id AS questionId,
         q.question,
+        q.question_image,
         q.option1,
+        q.option1_image,
         q.option2,
+        q.option2_image,
         q.option3,
+        q.option3_image,
         q.option4,
+        q.option4_image,
         q.option5,
+        q.option5_image,
         q.correct_option,
         a.selected_option
       FROM answers a
       JOIN questions q ON a.question_id = q.id
       WHERE a.\`Tələbə_kodu\` = ? AND a.\`Fənnin kodu\` = ?
     `,
-      [studentId, subjectCode]
+      [studentId, subjectCode],
     );
 
     res.json(review);
